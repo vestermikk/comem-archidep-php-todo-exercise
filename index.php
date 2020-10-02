8871184effd688472c240d049ec0e1fdffebfaa1
Mikkel Vestergaard <mikkel.vestergaard@heig-vd.ch>
2020-10-02 17:09:17 +0200

    Added Delete and Select queries

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $deleteQuery = ''; // IMPLEMENT ME
+        $deleteQuery = "DELETE FROM todo WHERE id = $id"; // IMPLEMENT ME
         if(!$db->query($deleteQuery)) {
           die(print_r($db->errorInfo(), true));
         }
@@ -70,7 +70,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME
+$selectQuery = 'SELECT * FROM todo'; // IMPLEMENT ME
 $items = $db->query($selectQuery);
 ?>
 
